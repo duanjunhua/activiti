6313edcfff64006c78baf1d5b9c4bceb6d690787
duanjunhua <1585115894@qq.com>
2019-04-16 10:08:18 +0800

    Activiti6.0 demo show work flow

--- activity-demo/src/com/michael/activiti/BpmnDemo.java
+++ activity-demo/src/com/michael/activiti/BpmnDemo.java
@@ -11,39 +11,39 @@ import org.activiti.engine.task.Task;
 public class BpmnDemo {
 
         public static void main(String[] args) {
-                // TODO Auto-generated method stub
-                
-                //1.Get the flow engine
-                ProcessEngine engine = ProcessEngines.getDefaultProcessEngine();
-                
-                //2.Get Repository Service, to save 
-                RepositoryService repoService = engine.getRepositoryService();
-                
-                //3.Get Runtime Service
-                RuntimeService runService = engine.getRuntimeService();
-                
-                //4.Get TaskService to handle take
-                TaskService taskService = engine.getTaskService();
-                
-                //Deploy Service, Can deploy multiple file at once
-                repoService.createDeployment().addClasspathResource("easy.bpmn").deploy();
+            // TODO Auto-generated method stub
+            
+            //1.Get the flow engine
+            ProcessEngine engine = ProcessEngines.getDefaultProcessEngine();
+            
+            //2.Get Repository Service, to save 
+            RepositoryService repoService = engine.getRepositoryService();
+            
+            //3.Get Runtime Service
+            RuntimeService runService = engine.getRuntimeService();
+            
+            //4.Get TaskService to handle take
+            TaskService taskService = engine.getTaskService();
+            
+            //Deploy Service, Can deploy multiple file at once
+            repoService.createDeployment().addClasspathResource("easy.bpmn").deploy();
 
-        //start flow process, return flow instance
-        ProcessInstance instance = runService.startProcessInstanceByKey("easy");
-        while(true) {
-                //query the task by taskService
-                Task task = taskService.createTaskQuery().processInstanceId(instance.getId()).singleResult();
-                if(null == task) break;
-                
-                System.out.println("Current task: " + task.getAssignee() + " : " + task.getName());
-                //Completed the task
-                taskService.complete(task.getId());
-        } 
-        
-        //Close engine
-        engine.close();
-        
-        //exit flow
-        System.exit(0);
+	        //start flow process, return flow instance
+	        ProcessInstance instance = runService.startProcessInstanceByKey("easy");
+	        while(true) {
+	                //query the task by taskService
+	                Task task = taskService.createTaskQuery().processInstanceId(instance.getId()).singleResult();
+	                if(null == task) break;
+	                
+	                System.out.println("Current task: " + task.getAssignee() + " : " + task.getName());
+	                //Completed the task
+	                taskService.complete(task.getId());
+	        } 
+	        
+	        //Close engine
+	        engine.close();
+	        
+	        //exit flow
+	        System.exit(0);
         }
 }
